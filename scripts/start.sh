5817cb7b7af32e105d7c905286fc7e3e72f05623
Jingwei.Qi <jingwei.qi@datayes.com>
2019-01-08 13:43:11 +0800

    flume 调试打包

--- scripts/start.sh
+++ scripts/start.sh
@@ -17,13 +17,7 @@ fi
 
 chmod 750 $BASEDIR/bin/flume-ng
 
-if [ $nodeType -eq 0 ]; then
-   echo "启动前置http source节点, flume信息监控端口34545."
-   nohup $BASEDIR/bin/flume-ng agent --conf $BASEDIR/conf --conf-file $BASEDIR/conf/flume-load_balance_node.properties --name balance -Dflume.root.logger=WARN,console -Dflume.monitoring.type=http -Dflume.monitoring.port=34545 >flume.load_balance.log 2>&1 &
-elif [ $nodeType -eq 1 ]; then
-   echo "启动收集进程, flume信息监控端口35545." 
-   nohup $BASEDIR/bin/flume-ng agent --conf $BASEDIR/conf --conf-file $BASEDIR/conf/flume-collector_node.properties --name producer -Dflume.root.logger=WARN,console -Dflume.monitoring.type=http -Dflume.monitoring.port=35545 >flume.collector.log 2>&1 &
-else
-   echo "节点类型参数不正确, 0:前置负载进程, 1:收集进程. 传入值为"$nodeType
-fi
+
+$BASEDIR/bin/flume-ng agent --conf $BASEDIR/conf --conf-file $BASEDIR/conf/flume-load_balance_node.properties --name balance -Dflume.root.logger=WARN,console -Dflume.monitoring.type=http -Dflume.monitoring.port=34545 >flume.load_balance.log 2>&1 &
+
 
