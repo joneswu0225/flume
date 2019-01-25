f367dd5da04fe64d4f5201de0828ee5391dc7f17
Jingwei.Qi <jingwei.qi@datayes.com>
2019-01-25 17:05:48 +0800

    flume Docker环境调试

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,13 +3,13 @@ set -e
 source /usr/bin/datayes-init
 ls -l
 
-curl ${params}/flume/flume-load_balance_node.properties?raw > flume-load_balance_node.properties
+curl ${params}/flume/flume-load_balance_node.properties?raw > conf/flume-load_balance_node.properties
 
 ls -l conf/
 while read line
 do
     echo ${line}
-done < config/application.properties
+done < flume-load_balance_node.properties
 echo "finish export envs"
 
 ./start.sh
