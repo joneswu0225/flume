88299df3db24e4da2bca8802954ba81c549f4870
Jingwei.Qi <jingwei.qi@datayes.com>
2019-01-26 11:11:07 +0800

    flume Docker环境调试

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
+# 详情见 http://confluence.datayes.com/pages/viewpage.action?pageId=24242117
 source /usr/bin/datayes-init
+
 ls -l
 
 curl ${params}/applog-flume/flume-load_balance_node.properties?raw > conf/flume-load_balance_node.properties
