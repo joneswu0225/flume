ef47353da8fc6ec78b34fdf77f4fcb489141da6e
Jingwei.Qi <jingwei.qi@datayes.com>
2019-01-28 17:58:08 +0800

    拉取flume镜像，将程序中flume相关去掉

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,6 +11,6 @@ echo "basedir,"$BASEDIR
 chmod 750 $THISDIR/bin/flume-ng
 
 echo "启动收集进程, flume信息监控端口34545."
-$THISDIR/bin/flume-ng agent --conf $THISDIR/conf --conf-file $THISDIR/conf/flume-load_balance_node.properties --name balance -Dflume.root.logger=INFO,console -Dflume.monitoring.type=http -Dflume.monitoring.port=34545 >flume.collector.log
+$THISDIR/bin/flume-ng agent --conf $THISDIR/conf --conf-file $THISDIR/conf/flume-load_balance_node.properties --name balance -Dflume.root.logger=INFO,console -Dflume.monitoring.type=http -Dflume.monitoring.port=34545 >flume.collector.log 2>&1
 
 
